baa0ac14e19870dd0d7cfb7172ebe1da6dd2411d
Aryokonon <arnon.yokoyama@gmail.com>
2023-10-21 12:51:51 +0900

    applick creation first commit

---
+++ app/controllers/applick_main_controller.rb
@@ -0,0 +1,8 @@
+class ApplickMainController < ApplicationController
+  def index
+    # respond_to do |format|
+    #   format.html { render plain: 'This is the HTML response' }
+    #   format.json { render json: { message: 'This is the JSON response' } }
+    # end
+end
+end
